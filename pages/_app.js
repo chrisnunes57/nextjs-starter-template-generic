5055b1ab8c83de24e9c55b0108bd50bb53b317ee
Chris Nunes <chrisnunes57@gmail.com>
2022-01-04 17:31:32 -0600

    initial commit, basic navigation and structure exists

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Navigation from '../components/Navigation'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Navigation />
+    
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
+  </>
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+function About() {
+    return (
+        <main>
+            <Head>
+                <title key="title">About</title>
+            </Head>
+            <h1 className="">About</h1>
+            <p className="">about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! about me! </p>
+        </main>
+    )
+}
+
+export default About
